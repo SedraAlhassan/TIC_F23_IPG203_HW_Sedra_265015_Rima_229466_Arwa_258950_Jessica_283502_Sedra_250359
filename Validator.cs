54b0e6a2410223f13fd1678451591702dbf19f4b
arwaalmassre <arwa.sedra1@gmail.com>
2025-10-16 22:34:46 +0300

    Add files via upload

---
+++ Validator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrarySystem.Utils
+{
+    public static class Validator
+    {
+        public static bool ValidateTitle(string title)
+        {
+            return !string.IsNullOrWhiteSpace(title);
+        }
+    }
+}
