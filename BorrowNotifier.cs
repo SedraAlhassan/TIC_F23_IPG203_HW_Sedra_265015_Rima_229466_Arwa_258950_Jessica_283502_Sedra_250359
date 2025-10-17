f49a7648718fd47203ed82b5b45b23441c57f5c2
sedraobaid990-prog <sedraobaid990@gmail.com>
2025-10-17 16:20:06 +0300

    Add files via upload

---
+++ BorrowNotifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using LibrarySystem.Models;
+
+namespace LibrarySystem.Services
+{
+    public delegate void BorrowHandler(string message);
+
+    public class BorrowNotifier
+    {
+        public event BorrowHandler OnBorrowed;
+
+        public void BorrowItem(LibraryItem item)
+        {
+            item.Borrow();
+            OnBorrowed?.Invoke($"{item.Title} has been borrowed!");
+        }
+    }
+}
