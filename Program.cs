5ff89d4a0c82332528c5821de18bb3949fc05cfa
Rima9902 <ghalia.ow99@gmail.com>
2025-10-18 21:44:20 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace LibrarySystem
+﻿using System;
+using LibrarySystem.Models;
+using LibrarySystem.Services;
+using LibrarySystem.Utils;
+
+namespace LibrarySystem
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Library lib = new Library();
+            Book b1 = new Book("B1", "C# Basics", "Mohamed Ali", 275);
+            Magazine m1 = new Magazine("M1", "Tech Monthly", "Samer Samer", 45);
+            Thesis t1 = new Thesis("T1", "AI in Healthcare", "Ali Ahmed", "SVU");
+
+            lib.AddItem(b1);
+            lib.AddItem(m1);
+            lib.AddItem(t1);
+            lib.ShowAllItems();
+            Console.WriteLine(Validator.ValidateTitle("Hello World") ? "Valid Title" : "Invalid Title");
+
+            BorrowNotifier notifier = new BorrowNotifier();
+            notifier.OnBorrowed += (msg) => Console.WriteLine($"Event Triggered: {msg}");
+            notifier.BorrowItem(b1);
+
+            ItemCounter c1 = new ItemCounter();
+            ItemCounter c2 = new ItemCounter();
+            Console.WriteLine($"Total Items Counted: {ItemCounter.TotalItems}");
         }
     }
 }
